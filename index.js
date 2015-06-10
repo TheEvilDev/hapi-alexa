c7d7587fa9e4f79e5111144b16bf0d6dcd869eb8
Mitchell Lee <mitchlee@amazon.com>
2015-06-09 18:17:12 -0700

    Initial version

---
+++ index.js
@@ -0,0 +1,37 @@
+exports.register = function(server, options, next) {
+    var endpoint = options.endpoint || '/';
+    var alexaService = options.service;
+
+    server.route({
+        method: 'POST',
+        path: endpoint,
+        handler: function(req, reply) {
+            var payload = req.payload;
+            var type = payload.request.type;
+
+
+            if(type === 'LaunchRequest') {
+                var response = alexaService.launch();
+                reply(response);
+            }
+
+            if(type === 'IntentRequest') {
+            var intent = payload.request.intent;
+
+            alexaService[intent.name](intent.slots)
+                .then(function(response) {
+                    reply(response);
+                });
+            }
+        },
+        config: {
+            description: 'Takes in requests posted from Amazon Alexa.'
+        }
+    });
+
+    next();
+};
+
+exports.register.attributes = {
+    pkg: require('./package.json')
+};
